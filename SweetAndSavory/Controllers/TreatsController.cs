5414a8f34191f991fef4b743283a425e17a02288
Ryan Walker <ryandanielwalker@gmail.com>
2021-06-04 09:11:12 -0700

    Add Treats index method and view

---
+++ SweetAndSavory/Controllers/TreatsController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using SweetAndSavory.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace SweetAndSavory.Controllers
+{
+  [Authorize]
+  public class TreatsController : Controller
+  {
+    private readonly SweetAndSavoryContext _db;
+    private readonly UserManager<User> _userManager;
+
+    public TreatsController(UserManager<User> userManager, SweetAndSavoryContext db)
+    {
+      _userManager = userManager;
+      _db = db;
+    }
+    [AllowAnonymous]
+    public ActionResult Index()
+    {
+      return View(_db.Treats.ToList());
+    }
+  }
+}
