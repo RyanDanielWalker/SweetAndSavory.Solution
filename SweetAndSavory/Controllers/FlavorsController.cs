b2b24a2bf029f5370326e486c3dc6423490646f1
Ryan Walker <ryandanielwalker@gmail.com>
2021-06-04 16:55:52 -0700

    WIP: Continue work on button styling. Prepare for push

--- SweetAndSavory/Controllers/FlavorsController.cs
+++ SweetAndSavory/Controllers/FlavorsController.cs
@@ -78,7 +78,7 @@ namespace SweetAndSavory.Controllers
     public ActionResult AddTreat(int id)
     {
       var thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
-      ViewBag.TreatId = new SelectList(_db.Flavors, "TreatId", "Name");
+      ViewBag.TreatId = new SelectList(_db.Treats, "TreatId", "Name");
       return View(thisFlavor);
     }
 
